99ec62a4e5cd9d8c1910d546c129750298d31834
Sanju2244 <103499120+Sanju2244@users.noreply.github.com>
2024-07-04 10:38:54 +0530

    updated provider

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.5.0"
+      version = "~> 4.0"
     }
-  }
 }
-
+}
 provider "aws" {
   region = "ap-south-1"
 }
