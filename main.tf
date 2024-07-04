1ff3931d1c27970819b5a3e1465f5cd7949c154a
Sanju2244 <103499120+Sanju2244@users.noreply.github.com>
2024-07-04 10:51:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-01b799c439fd5516a" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
 }
